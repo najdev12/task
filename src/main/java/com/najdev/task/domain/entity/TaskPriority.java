a43c41c5a6212c8dc0206239556beb895428b2b3
najima <sahloumi.najima@gmail.com>
2026-09-15 11:42:42 +0200

    TaskStatus and TaskPriority enums added

---
+++ src/main/java/com/najdev/task/domain/entity/TaskPriority.java
@@ -0,0 +1,7 @@
+package com.najdev.task.domain.entity;
+
+public enum TaskPriority {
+    HIGH,
+    MEDIUM,
+    LOW
+}
---
+++ src/main/java/com/najdev/task/domain/entity/TaskStatus.java
@@ -0,0 +1,6 @@
+package com.najdev.task.domain.entity;
+
+public enum TaskStatus {
+    OPEN,
+    COMPLETE
+}
