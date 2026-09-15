916bfa12dbff5407cfb901f5da21c872848c8c09
najima <sahloumi.najima@gmail.com>
2026-09-15 17:48:11 +0200

    Create CreateTaskRequest class

---
+++ src/main/java/com/najdev/task/domain/CreateTaskRequest.java
@@ -0,0 +1,15 @@
+package com.najdev.task.domain;
+
+import com.najdev.task.domain.entity.TaskPriority;
+
+import java.time.LocalDate;
+
+public record CreateTaskRequest(
+        String title,
+        String description,
+        LocalDate dueDate,
+        TaskPriority priority
+) {
+
+
+}
