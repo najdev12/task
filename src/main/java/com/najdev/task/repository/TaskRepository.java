1efa8d7559314abaeead0adecda7c7537fea5d27
najima <sahloumi.najima@gmail.com>
2026-09-15 17:17:08 +0200

    Task repository interface created

---
+++ src/main/java/com/najdev/task/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.najdev.task.repository;
+
+import com.najdev.task.domain.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+}
